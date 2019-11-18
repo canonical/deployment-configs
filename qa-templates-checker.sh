60e7c2c6534042692a8376775920bb901c68d9fa
Francisco Jimenez Cabrera <francisco.jimenez.cabrera@canonical.com>
2019-11-18 16:11:49 +0000

    Delete developer.ubuntu.com

--- qa-templates-checker.sh
+++ qa-templates-checker.sh
@@ -6,22 +6,6 @@
 # Bash strict mode
 set -euo pipefail
 
-parse_yaml() {
-   local prefix=$2
-   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
-   sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
-        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
-   awk -F$fs '{
-      indent = length($1)/2;
-      vname[indent] = $2;
-      for (i in vname) {if (i > indent) {delete vname[i]}}
-      if (length($3) > 0) {
-         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
-      }
-   }'
-}
-
 # Remove older log file
 if [[ -f ./diff.log ]]; then
   rm ./diff.log
